9d52f1e89926b6eee147afe08c48fd40ad65306e
Fiifi Pius <fiifipius@gmail.com>
2020-08-02 13:35:34 +0000

    Code refactor

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,32 +1,32 @@
-//import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 //import 'package:geolocator/geolocator.dart';
-//
-//class LoadingScreen extends StatefulWidget {
-//  @override
-//  _LoadingScreenState createState() => _LoadingScreenState();
-//}
-//
-//class _LoadingScreenState extends State<LoadingScreen> {
-//  @override
-//  void initState() {
-//    super.initState();
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
 //    getLocation();
-//  }
+  }
 //  void getLocation() async{
 //    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
 //    print(position);
 //  }
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      body: Center(
-//        child: RaisedButton(
-//          onPressed: () {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
 //            getLocation();
-//          },
-//          child: Text('Get Location'),
-//        ),
-//      ),
-//    );
-//  }
-//}
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
