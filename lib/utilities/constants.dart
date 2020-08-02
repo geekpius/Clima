4059de6cfc810f44f0372f811cccf5994570092a
Fiifi Pius <fiifipius@gmail.com>
2020-08-02 13:31:20 +0000

    Directory structured

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const kapiKey = "5a5f83241b9bee115ba20fe2c459b1f8";
